ad28b53f9fbe7265c0100aecbf69527a03048f0a
brandshore <szabixsz@gmail.com>
2021-10-19 21:09:16 +0200

    test

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection(props) {
       className="content__row content__row--full-width promo__section"
       data-id={_.get(section, "section_id", null)}
     >
-      {/* <h1 class="text-xl sm:text-center my-20">
+      <h1 class="text-xl sm:text-center my-20">
         <Typewriter
           options={{
             strings: [
@@ -25,7 +25,7 @@ export default function HeroSection(props) {
             loop: true,
           }}
         />
-      </h1> */}
+      </h1>
       <div className="hero-wave">
         <div class="ocean">
           <div class="wave"></div>
