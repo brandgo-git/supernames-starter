900f0a2f1ed6bea292c3db4242d9bae224286552
brandshore <szabixsz@gmail.com>
2021-10-19 19:00:55 +0200

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import TextTransition, { presets } from "react-text-transition"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import _ from "lodash"
@@ -6,11 +7,62 @@ import "../css/wave.css"
 
 export default function HeroSection(props) {
   let section = _.get(props, "section", null)
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex(index => index + 1),
+      3000 // every 3 seconds
+    )
+    return () => clearTimeout(intervalId)
+  }, [])
+
+  const TEXTS = [
+    _.get(section, "looptext_1", null),
+    _.get(section, "looptext_2", null),
+    _.get(section, "looptext_3", null),
+    _.get(section, "looptext_4", null),
+  ]
+
   return (
     <section
       className="content__row content__row--full-width promo__section"
       data-id={_.get(section, "section_id", null)}
     >
+      {/* <h1>
+        {_.get(section, "before", null)}{" "}
+        <TextTransition
+          inline={true}
+          delay={100}
+          noOverflow
+          text={TEXTS[index % TEXTS.length]}
+          springConfig={presets.gentle}
+        />
+        <span class="hero-title">{_.get(section, "title", null)}</span>{" "}
+        <span>{_.get(section, "after", null)} </span>
+        <span sx={{ display: `inline-block` }}>
+          <TypistLoop interval={1000}>
+            {[
+              _.get(section, "looptext_1", null),
+              _.get(section, "looptext_2", null),
+              _.get(section, "looptext_3", null),
+              _.get(section, "looptext_4", null),
+            ].map(text => (
+              <Typist key={text}>
+                {_.get(section, "before_loop", null)} {text}
+                <Typist.Backspace count={15} delay={2500} />
+              </Typist>
+            ))}
+          </TypistLoop>
+        </span>
+      </h1> */}
+      <TextTransition
+        inline={true}
+        delay={100}
+        noOverflow
+        text={TEXTS[index % TEXTS.length]}
+        springConfig={presets.gentle}
+      />
       <div className="hero-wave">
         <div class="ocean">
           <div class="wave"></div>
